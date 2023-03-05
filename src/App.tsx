9d8c2347d6d9e248e6008deeb1b3bb933784a8c8
ExStas_Poh <monax071595@gmail.com>
2023-03-05 15:05:27 +0300

    Array money

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,38 @@ import {UniversButton} from "./components/Buttons/UniversButton";
 
 const App = () => {
 
+    const [money, setMoney] = useState([
+        {banknots: 'Dollars', value: 100, number: ' a1234567890'},
+        {banknots: 'Dollars', value: 50, number: ' z1234567890'},
+        {banknots: 'RUBLS', value: 100, number: ' w1234567890'},
+        {banknots: 'Dollars', value: 100, number: ' e1234567890'},
+        {banknots: 'Dollars', value: 50, number: ' c1234567890'},
+        {banknots: 'RUBLS', value: 100, number: ' r1234567890'},
+        {banknots: 'Dollars', value: 50, number: ' x1234567890'},
+        {banknots: 'RUBLS', value: 50, number: ' v1234567890'},
+    ])
+
+    return (
+        <li>
+            <></>
+        </li>
+
+    )
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 /*    const Button1Foo = (subscriber: string, age: number, adress: string) => {
         console.log(subscriber, age, adress)
     }
@@ -32,35 +64,22 @@ const App = () => {
     }*/
 
 
-    const [money, setMoney] = useState([
-        { banknots: 'Dollars', value: 100, number: ' a1234567890' },
-        { banknots: 'Dollars', value: 50, number: ' z1234567890' },
-        { banknots: 'RUBLS', value: 100, number: ' w1234567890' },
-        { banknots: 'Dollars', value: 100, number: ' e1234567890' },
-        { banknots: 'Dollars', value: 50, number: ' c1234567890' },
-        { banknots: 'RUBLS', value: 100, number: ' r1234567890' },
-        { banknots: 'Dollars', value: 50, number: ' x1234567890' },
-        { banknots: 'RUBLS', value: 50, number: ' v1234567890' },
-    ])
-
-
-
-    return (
+/*    return (
         <div>
-            {/*<button>MyYouTubeChanel_1</button>
-            <button>MyYouTubeChanel_1</button>*/}
+            {/!*<button>MyYouTubeChanel_1</button>
+            <button>MyYouTubeChanel_1</button>*!/}
 
-            {/*<UniversButton name={"MyYouTubeChanel_1"} callBack={()=>Button1Foo ("Im Vasya", 21, "live in Voronezh")}/>
+            {/!*<UniversButton name={"MyYouTubeChanel_1"} callBack={()=>Button1Foo ("Im Vasya", 21, "live in Voronezh")}/>
             <UniversButton name={"MyYouTubeChanel_2"} callBack={()=>Button2Foo ("Im Ivan")}/>
-            <UniversButton name={"Stupid button"} callBack={Button3Foo}/>*/}
+            <UniversButton name={"Stupid button"} callBack={Button3Foo}/>*!/}
 
-            {/*<h1>{a}</h1>
+            {/!*<h1>{a}</h1>
             <button onClick={onClickHandler}>number</button>
             <button onClick={onClickHandler2}>delete</button>
-            <button onClick={onClickHandler3}>reset</button>*/}
+            <button onClick={onClickHandler3}>reset</button>*!/}
 
         </div>
-    );
-}
+    );*/
+
 
 export default App;
