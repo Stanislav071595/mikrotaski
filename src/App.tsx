80731d4aa875b4c397f6a06012ed13f77fea9be6
ExStas_Poh <monax071595@gmail.com>
2023-03-09 17:41:49 +0300

    Filter

--- src/App.tsx
+++ src/App.tsx
@@ -5,38 +5,65 @@ import {CarTable} from "./map/MapMetod";
 import {UniversButton} from "./components/Buttons/UniversButton";
 
 
+
+
+type FilterType = 'All' | 'Dollars' | 'RUBLS'
+
 const App = () => {
 
     const [money, setMoney] = useState([
-        {banknots: 'Dollars', value: 100, number: ' a1234567890'},
-        {banknots: 'Dollars', value: 50, number: ' z1234567890'},
-        {banknots: 'RUBLS', value: 100, number: ' w1234567890'},
-        {banknots: 'Dollars', value: 100, number: ' e1234567890'},
-        {banknots: 'Dollars', value: 50, number: ' c1234567890'},
-        {banknots: 'RUBLS', value: 100, number: ' r1234567890'},
-        {banknots: 'Dollars', value: 50, number: ' x1234567890'},
-        {banknots: 'RUBLS', value: 50, number: ' v1234567890'},
+        {banknots: 'Dollars ', value: 100, number: ' a1234567890'},
+        {banknots: 'Dollars ', value: 50, number: ' z1234567890'},
+        {banknots: 'RUBLS ', value: 100, number: ' w1234567890'},
+        {banknots: 'Dollars ', value: 100, number: ' e1234567890'},
+        {banknots: 'Dollars ', value: 50, number: ' c1234567890'},
+        {banknots: 'RUBLS ', value: 100, number: ' r1234567890'},
+        {banknots: 'Dollars ', value: 50, number: ' x1234567890'},
+        {banknots: 'RUBLS ', value: 50, number: ' v1234567890'},
     ])
 
-    return (
-        <li>
-            <></>
-        </li>
+    let currentMoney = money;
 
+    const[filter, setFilter] = useState<FilterType>("All")
+
+    if(filter === 'Dollars'){
+        currentMoney = money.filter((filteredMoney) => filteredMoney.banknots === 'Dollars ')
+    }
+    if (filter === 'RUBLS'){
+        currentMoney = money.filter((filteredMoney) => filteredMoney.banknots === 'RUBLS ')
+    }
+
+
+
+
+
+    const OneClickFilterHandler = (nameButton: FilterType) => {
+        setFilter(nameButton)
+    }
+
+    return (
+
+        <>
+            <ul>
+                {currentMoney.map((ObjUseState, index) => {
+                    return (
+                        <li key={index}>
+                            <span>{ObjUseState.banknots}</span>
+                            <span>{ObjUseState.value}</span>
+                            <span>{ObjUseState.number}</span>
+                        </li>
+                    )
+                })}
+            </ul>
+            <div style={{marginLeft: "35px"}}>
+                <button onClick={()=>OneClickFilterHandler("All")}>All</button>
+                <button onClick={()=>OneClickFilterHandler("RUBLS")}>RUBLS</button>
+                <button onClick={()=>OneClickFilterHandler("Dollars")}>Dollars</button>
+            </div>
+
+        </>
     )
 }
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 /*    const Button1Foo = (subscriber: string, age: number, adress: string) => {
